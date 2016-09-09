cf1272e6d616656676739638c0051421c53bc495
Christian Tzolov <christian.tzolov@gmail.com>
2016-09-09 09:33:59 +0200

    PostgreSQL setup appendix

--- start-scdf-pcfdev.sh
+++ start-scdf-pcfdev.sh
@@ -19,26 +19,5 @@ export SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_STREAM_BUILDPACK=java_buildpack
 # export MAVEN_OFFLINE=true
 export MAVEN_REMOTE_REPOSITORIES_REPO1_URL=https://repo.spring.io/release
 
-scdf_jar="./spring-cloud-dataflow-server-cloudfoundry-1.0.0.RELEASE.jar"
-
-if [ -f "$scdf_jar" ]
-then
-	echo "$scdf_jar found."
-else
-	echo "Download: $scdf_jar "
-  wget http://repo.spring.io/libs-release/org/springframework/cloud/spring-cloud-dataflow-server-cloudfoundry/1.0.0.RELEASE/spring-cloud-dataflow-server-cloudfoundry-1.0.0.RELEASE.jar
-fi
-
-scdf_shel_jar="./pring-cloud-dataflow-shell-1.0.0.RELEASE.jar"
-
-if [ -f "$scdf_shel_jar" ]
-then
-	echo "$scdf_shel_jar found."
-else
-	echo "Download: $scdf_shel_jar "
-  wget http://repo.spring.io/libs-release/org/springframework/cloud/spring-cloud-dataflow-shell/1.0.0.RELEASE/spring-cloud-dataflow-shell-1.0.0.RELEASE.jar
-fi
-
-
 java -jar ./spring-cloud-dataflow-server-cloudfoundry-1.0.0.RELEASE.jar
 
